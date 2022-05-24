90ac86611d59e2b905a9e8e50412947ca19320ea
Giko <glnjendu2004@gmail.com>
2022-05-23 21:36:19 -0500

    Add files via upload

---
+++ Lab Unit 5 - College/CollegeMapping.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)CollegeMapping.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/10/22
+ */
+
+
+public class CollegeMapping {
+	 private static final String UNDECIDED = "UNDECIDED";
+    public CollegeMapping() {
+    }
+    
+    
+}
